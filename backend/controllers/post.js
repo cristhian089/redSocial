2574b258afb970698a641d64dcfbf353beca3bd3
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-18 17:34:07 -0500

    midified controllers post, adding listPost

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -5,7 +5,6 @@ const registerPost = async (req, res) => {
   if (!req.body.text || !req.body.hashtag)
     return res.status(400).send("Process failed: Incomplete data");
 
-
   let post = new Post({
     userId: req.user._id,
     text: req.body.text,
@@ -18,7 +17,7 @@ const registerPost = async (req, res) => {
 };
 
 const listPost = async (req, res) => {
-  let post = await Post.find({ name: new RegExp(req.params["userId"], "i") })
+  let post = await Post.find({ userId : req.user._id })
     .populate("userId")
     .exec();
   if (!post || post.length === 0) return res.status(400).send("No post");
