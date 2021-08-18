06998088d23d71bbdda90dec1d35206785b174e5
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-18 10:15:50 -0500

    create project red social and module user

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./db/db");
+const User = require("./routes/user");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/user",User);
+
+app.listen(process.env.PORT, () =>
+  console.log("Backend server running on port: ", process.env.PORT)
+);
+
+dbConnection();
---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose= require("mongoose");
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    dbStatus: Boolean,
+});
+
+userSchema.methods.generateJWT = function(){
+    return jwt.sign({
+        _id: this._id,
+        name: this.name,
+    },
+    process.env.SECRECT_KEY_JWT
+    );
+};
+
+const user = mongoose.model("user",userSchema);
+module.exports = user;
